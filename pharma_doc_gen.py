e50e9daa93ce37abe1cdd8837c5f474ca7098042
SaleemDM <dmsaleem8@gmail.com>
2025-05-12 17:10:59 +0530

    Update pharma_doc_gen.py

--- pharma_doc_gen.py
+++ pharma_doc_gen.py
@@ -1,3 +1,4 @@
+st.markdown("""<meta name="google-site-verification" content="googlea7a6900fa5d8b9a3.html" />""", unsafe_allow_html=True)
 import streamlit as st
 import datetime
 from enum import Enum
